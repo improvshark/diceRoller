91f209a92144291dc5b83dbd840a56da2018bdc3
stephen boettcher <moonyou1234321@gmail.com>
2012-04-03 23:00:00 -0700

    woot

--- test1/die.cpp
+++ test1/die.cpp
@@ -6,18 +6,9 @@
 
 Die::Die(const char *name, int sides) : Gtk::Button(name)
 {
-<<<<<<< HEAD
-	
-	set_size_request(50, 50); // Make buttons square
-	
-	// set seed for random number
-	srand ( time(NULL));
-=======
-
 	set_size_request(50, 50); // Make buttons square
 
 	srand( time(NULL) );
->>>>>>> d28686e9c23e97f3532075dfedcd8294e43015c8
 	
 	// Make sure there are at least 2 sides
 	m_sides = sides >= 2 ? sides : 2;
